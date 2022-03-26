0d757d315014ab73681a5bb5770773d9793403b7
Weslley de Campos <weslleydrum@hotmail.com>
2022-03-26 16:58:24 -0300

    test: ensure transfer-founds is working

--- test/03-transfer-founds.spec.ts
+++ test/03-transfer-founds.spec.ts
@@ -1,4 +1,4 @@
-import { test, describe } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
 test.describe('[Transfer founds and make payments]', () => {
   test.beforeEach(async ({ page }) => {
@@ -10,6 +10,22 @@ test.describe('[Transfer founds and make payments]', () => {
     await page.click('text=Sign in')
   })
   test('expect userMake transfer', async({ page }) => {
-    await page.click('', '')
+    await page.click('#transfer_founds_tab')
+    await page.selectOption('#tf_fromAccountId', '2')
+    await page.selectOption('#td_toAccountId', '3')
+
+    await page.type('#tf_amount', '588')
+
+    await page.type('#tf_description', 'any_description')
+    await page.click('#btn_submit')
+    
+    const boarderText = page.locator('h2.board-header')
+
+    await expect(boarderText).toContainText('Verify')
+
+    await page.click('#btn_submit')
+    const message = page.locator('.alert-success')
+
+    await expect(message).toContainText('You successfully submitted your transaction')
   })
 })
---
+++ test/04-show-transactions.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test'
+
+
+test.describe('[Show transactions]', () => {
+  test.beforeEach(async () => {})
+})
